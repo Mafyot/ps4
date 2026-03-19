f59390c79661ede02c05de2c6513383d72af4506
Mafyot <mafyot25@gmail.com>
2026-03-19 22:30:33 +0100

    Update lapse.mjs

--- psfree900/lapse.mjs
+++ psfree900/lapse.mjs
@@ -1711,8 +1711,8 @@ export async function kexploit() {
     
      if (localStorage.ExploitLoaded === "yes" && sessionStorage.ExploitLoaded!="yes") {
 		   setTimeout(PayloadLoader("aio_patches.bin"),500);
-		   setTimeout(PayloadLoader("goldhen_2.4b18.6.bin"),500);
-		   msgs.innerHTML = "GoldHEN v2.4b18.6 Loaded ...";
+		   setTimeout(PayloadLoader("goldhen.bin"),500);
+		   msgs.innerHTML = "GoldHEN Loaded ...";
 		   return new Promise(() => {});
       }
  
@@ -1868,7 +1868,7 @@ kexploit().then(() => {
 setTimeout(PayloadLoader("aio_patches.bin"),500);
 log("AIO Fixes Applied.!");
 //Load GoldHEN :)
-setTimeout(PayloadLoader("goldhen_2.4b18.9.bin"),500);
-msgs.innerHTML = "GoldHEN v2.4b18.9 Loaded ...";
+setTimeout(PayloadLoader("goldhen.bin"),500);
+msgs.innerHTML = "GoldHEN Loaded ...";
 
 })
